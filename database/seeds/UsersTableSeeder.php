981a1bfe47e33e5f00089104cac7fbd8bb36ddef
DioBrandoLong <niu280974497@gmail.com>
2017-09-21 09:02:59 +0000

    D

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,23 @@
+<?php
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $users=factory(User::class);
+        User::insert($users->makeVisible(['password', 'remember_token'])->toArray());
+
+        $user=User::find(1);
+        $user->name='Aufree';
+        $user->email='aufree@yousails.com';
+        $user->password=bcrypt('password');
+        $user->save();
+    }
+}
